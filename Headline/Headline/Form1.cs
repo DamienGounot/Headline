67259aa0d7adf8f1f6d4e6708f253abcb326f13a
Tnecnivm <vinc.monnerie@gmail.com>
2019-06-23 20:26:51 +0200

    Final API

--- Headline/Headline/Form1.cs
+++ Headline/Headline/Form1.cs
@@ -18,6 +18,34 @@ namespace Headline
 {
     public partial class Form1 : Form
     {
+
+        #region init
+        string source1 = "";
+        string author1 = "";
+        string date1 = "";
+        string description1 = "";
+        string title1 = "";
+        string content1 = "";
+        string image1;
+
+        string source2 = "";
+        string author2 = "";
+        string date2 = "";
+        string description2 = "";
+        string title2 = "";
+        string content2 = "";
+        string image2;
+
+        string source3 = "";
+        string author3 = "";
+        string date3 = "";
+        string description3 = "";
+        string title3 = "";
+        string content3 = "";
+        string image3;
+        #endregion
+
+
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +56,8 @@ namespace Headline
 
         }
 
+
+        #region Login Page
         // Login Page
 
         private void button_login_Click_1(object sender, EventArgs e)
@@ -77,6 +107,9 @@ namespace Headline
 
         }
 
+        #endregion
+
+        #region Registration page
 
         // Registration Page
 
@@ -157,6 +190,10 @@ namespace Headline
 
         }
 
+
+        #endregion
+
+        #region Home page
         // Home Page
 
         private void btn_Search_Click_1(object sender, EventArgs e)
@@ -176,28 +213,38 @@ namespace Headline
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            showarticle(source1, author1, content1, date1, description1, title1,image1);
             this.Text = "Article";
             preferences_panel.Visible = true;
             search_panel.Visible = true;
             panel_article.Visible = true;
+            
+            
+
         }
 
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            showarticle(source2, author2, content2, date2, description2, title2,image2);
             this.Text = "Article";
             preferences_panel.Visible = true;
             search_panel.Visible = true;
             panel_article.Visible = true;
+            
         }
 
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            showarticle(source3, author3, content3, date3, description3, title3,image3);
             this.Text = "Article";
             preferences_panel.Visible = true;
             search_panel.Visible = true;
             panel_article.Visible = true;
         }
 
+        #endregion
+
+        #region Preferences page
         // Preferences Page
 
         private void butn_home_pref_Click_1(object sender, EventArgs e)
@@ -212,10 +259,10 @@ namespace Headline
             MessageBox.Show("Not Implemented Yet");
 
         }
+        #endregion
 
-
+        #region Search page
         // Search Page
-
 
         private void button_home_from_search_Click_1(object sender, EventArgs e)
         {
@@ -226,18 +273,29 @@ namespace Headline
 
         private void button_make_search_Click_1(object sender, EventArgs e)
         {
-           // MessageBox.Show("Not Implemented Yet");
-
             callapi();
-
         }
 
 
+        private void showarticle(string source, string author, string content, string date, string description, string title,string image)
+        {
+            callapi();
+            article_source.Text = source;
+            article_author.Text = author;
+            article_content.Text = content;
+            article_date.Text = date;
+            article_description.Text = description;
+            article_title.Text = title;
+            pictureBox6.ImageLocation = image;
+
+        }
+
         #region newsapi
         private void callapi(){
-            
-       
-          
+
+            var errors = 0;
+            var param=0;
+
             #region keyword
 
             var keyword = "";
@@ -251,6 +309,8 @@ namespace Headline
             {
                 keyword = textBox_keyword_search.Text;
                 totalkeyword = "q=" + keyword + "&";
+                param = param + 1;
+                
             }
 
             #endregion
@@ -266,6 +326,8 @@ namespace Headline
             {
                 country = textBox_country_search.Text;
                 totalcountry = "country=" + country + "&";
+                errors = errors + 1;
+                param = param + 1;
             }
             #endregion
 
@@ -281,48 +343,110 @@ namespace Headline
             {
                 source = textBox_source_search.Text;
                 totalsource= "sources=" + source + "&";
+                errors = errors + 1;
+                param = param + 1;
             }
 
             #endregion
-            
-            var url = "https://newsapi.org/v2/top-headlines?" + totalkeyword+
+
+            if (errors >= 2)
+            {
+                MessageBox.Show("You cannot search with country and source parameters at the same time");
+            }else if (param<1)
+            {
+                MessageBox.Show("You need to have one parameter at least");
+            }
+            else
+            {
+
+                var url = "https://newsapi.org/v2/top-headlines?" + totalkeyword +
                     totalcountry + totalsource +
                     "apiKey=fba415c197974798bd1833b9f86de604";
-            MessageBox.Show(url);
-            var json = new WebClient().DownloadString(url);
+                MessageBox.Show(url);
+                var json = new WebClient().DownloadString(url);
 
 
 
-            var jPerson = JsonConvert.DeserializeObject<dynamic>(json);
+                var jPerson = JsonConvert.DeserializeObject<dynamic>(json);
+
+                var i = 1;
+                foreach (var num in jPerson.articles)
+                {
+                   
+
+                    if (i == 1)
+                    {
+                        source1 = num.source.name;
+                        author1 = num.author;
+                        content1 = num.content;
+                        date1 = num.publishedAt;
+                        description1 = num.description;
+                        title1 = num.title;
+                        image1 = num.urlToImage;
+
+                        pictureBoxArticle1.ImageLocation = num.urlToImage;
+                        titlehome1.Text = num.title;
+                  
 
 
-            foreach(var num in jPerson.articles)
-            {
-                article_source.Text = num.source.name;
-                article_author.Text = num.author;
-                article_content.Text = num.content;
-                article_date.Text = num.publishedAt;
-                article_description.Text = num.description;
-                article_title.Text = num.title;
+
+
+                    }else if (i == 2)
+                    {
+
+                        source2 = num.source.name;
+                        author2 = num.author;
+                        content2 = num.content;
+                        date2 = num.publishedAt;
+                        description2 = num.description;
+                        title2 = num.title;
+                        image2 = num.urlToImage;
+
+                        pictureBoxArticle2.ImageLocation = num.urlToImage;
+                        titlehome2.Text = num.title;
+                    }
+                    else if (i == 3)
+                    {
+
+                        source3 = num.source.name;
+                        author3 = num.author;
+                        content3 = num.content;
+                        date3 = num.publishedAt;
+                        description3 = num.description;
+                        title3 = num.title;
+                        image3 = num.urlToImage;
+
+                        pictureBoxArticle3.ImageLocation = num.urlToImage;
+                        titlehome3.Text = num.title;
+                    }
+
+                    i++;
+                    
+
+                }
+
 
             }
+            
+            
             
            
         }
 
         #endregion
 
-
+        
 
         private void button_from_search_to_pref_Click(object sender, EventArgs e)
         {
             this.Text = "Preferences";
             search_panel.Visible = false;
         }
+        #endregion
 
+
+        #region Article page
         // Article Page
-
-
         private void btn_article_toSearch_Click_1(object sender, EventArgs e)
         {
             this.Text = "Search";
@@ -336,7 +460,7 @@ namespace Headline
             search_panel.Visible = false;
         }
 
-        private void btn_article_toHome_Click_1(object sender, EventArgs e)
+        private void btn_article_toHome_Click(object sender, EventArgs e)
         {
             this.Text = "Home";
             panel_article.Visible = false;
@@ -344,5 +468,15 @@ namespace Headline
             preferences_panel.Visible = false;
             Home_Panel.Visible = true;
         }
+
+
+        #endregion
+
+        private void panel_article_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
     }
 }
